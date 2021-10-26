ffdda488d056a252b07c6ce783e89ad4360a156c
Hailey <haileystrobelt@gmail.com>
2021-10-25 21:19:25 -0600

    Macro Script

--- Clayton 3670/Notes/PythonVidNotes_9-9.py
+++ Clayton 3670/Notes/PythonVidNotes_9-9.py
@@ -1,10 +1,11 @@
 import maya.cmds as cmds
 
+cmds.polySphere()
 
 #Delete cubes in the scene w/o having to do it manually.
 cubeList = cmds.ls('myCube*') #aterick symbol will allow any text
 #after myCube is acceptable. I.e. ['myCube1', 'myCube1Shape', 'myCube2', ... etc.]
-if len(cubeList) > 0;
+if len(cubeList) > 0:
     cmds.delete(cubeList)
 
 
