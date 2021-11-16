4001a50c65af630e3fa1fbf8698f42c01e6bc0ea
Hailey <haileystrobelt@gmail.com>
2021-11-16 15:08:47 -0700

    Animation Work

--- Clayton 3670/Color_Control/Color_Control.py
+++ Clayton 3670/Color_Control/Color_Control.py
@@ -1,16 +1,21 @@
 import maya.cmds as cmds
 
-def colorChange(i, control):
+
+# put sphere in a group
+cmds.group( 'sphere1', n='group1' )
+
+def colorChange(i, ctrl):
     colorList = ['pewter', 'black', 'iron', 'gray', 'red', 'navy blue', "indigo (crayola)", 'juniper', 'indigo',
                  'purple', 'tawny', 'hickory', 'cinnamon', 'scarlet', 'pear', 'sapphire', 'white', 'yellow',
                  'baby blue', 'sage', 'pink', 'tortilla', 'corn', 'hunter', 'peanut', 'sheen green', 'venom',
                  'dollar bill', 'turkish', 'maya blue', 'violet', 'magenta']
+    cmds.setAttr('sphere.color', 5)
     # cmds.color('sphere1', ud=1)
     if i.isnumeric():  # if it can be converted to a number
-        cmds.color(control, ud=int(i))  # use index, make sure it's converted though
+        cmds.setAttr((ctrl + ".overrideEnabled"), int(i))  # use index, make sure it's converted though
     if i in colorList:
         i = colorList.index(i)  # if the index is in the list, find which index it's at
-        cmds.color(control, ud=int(i))  # use that index to get the color in maya
+        cmds.setAttr((ctrl + ".overrideEnabled"), int(i))  # use that index to get the color in maya
     else:
         print("Incorrect input. Please provide a number 0-31. Or provide a name within the following list: \n")
         print(colorList)
@@ -27,6 +32,8 @@ def createControl(color_ind):
         ctrl = cmds.circle(n='Default_Ctrl')  # create and name nurbs circle
         cmds.select(ctrl)  # select it
         cmds.rotate('90deg', 0, 0, r=True)  # fix incorrect rotation
+        grp = cmds.group(ctrl, n='Grp_Crtl')
+
 
         colorChange(color_ind, ctrl) # change control's color
 
@@ -35,7 +42,8 @@ def createControl(color_ind):
         ctrl = cmds.circle(n=ctrl_name)  # create and name nurbs circle
         cmds.select(x)  # select it
         cmds.rotate('90deg', 0, 0, r=True)  # fix incorrect rotation
-        cmds.matchTransform(ctrl, sels[0]) # set control to match transforms of selection
+        grp = cmds.group(ctrl, n='Grp_Crtl')
+        cmds.matchTransform(grp, sels[0]) # set control to match transforms of selection
 
         colorChange(color_ind, ctrl)  # change control's color
 
@@ -45,6 +53,7 @@ def createControl(color_ind):
             ctrl = cmds.circle(n=ctrl_name)  # create and name nurbs circle
             cmds.select(x)  # select it
             cmds.rotate('90deg', 0, 0, r=True)  # fix incorrect rotation
+            grp = cmds.group(ctrl, n='Grp_Crtl')
             cmds.matchTransform(ctrl, sels[0])  # set control to match transforms of selection
 
             colorChange(color_ind, ctrl)  # change control's color
