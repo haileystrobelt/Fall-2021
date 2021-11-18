d9511cff59809d0b6cc2f7eaf025796b737ccf89
Hailey <haileystrobelt@gmail.com>
2021-11-18 09:56:36 -0700

    Color Control Changes

--- Clayton 3670/Color_Control/Color_Control.py
+++ Clayton 3670/Color_Control/Color_Control.py
@@ -1,24 +1,26 @@
 import maya.cmds as cmds
 
-
-# put sphere in a group
-cmds.group( 'sphere1', n='group1' )
-
 def colorChange(i, ctrl):
     colorList = ['pewter', 'black', 'iron', 'gray', 'red', 'navy blue', "indigo (crayola)", 'juniper', 'indigo',
                  'purple', 'tawny', 'hickory', 'cinnamon', 'scarlet', 'pear', 'sapphire', 'white', 'yellow',
                  'baby blue', 'sage', 'pink', 'tortilla', 'corn', 'hunter', 'peanut', 'sheen green', 'venom',
                  'dollar bill', 'turkish', 'maya blue', 'violet', 'magenta']
-    cmds.setAttr('sphere.color', 5)
     # cmds.color('sphere1', ud=1)
-    if i.isnumeric():  # if it can be converted to a number
-        cmds.setAttr((ctrl + ".overrideEnabled"), int(i))  # use index, make sure it's converted though
+    if isinstance(i, int):  # if it's an integer
+        if 0 <= i <= 31:
+            cmds.setAttr((ctrl[0] + '.overrideEnabled'), 1)
+            cmds.setAttr((ctrl[0] + '.overrideColor'), i)  # use index, make sure it's converted though
+        else:
+            print("Incorrect input. Please provide a number 0-31. Or provide a name within the following list: \n")
+            print(colorList)
     if i in colorList:
         i = colorList.index(i)  # if the index is in the list, find which index it's at
-        cmds.setAttr((ctrl + ".overrideEnabled"), int(i))  # use that index to get the color in maya
+        cmds.setAttr((ctrl[0] + '.overrideEnabled'), 1)
+        cmds.setAttr((ctrl[0] + '.overrideColor'), int(i))  # use that index to get the color in maya
     else:
-        print("Incorrect input. Please provide a number 0-31. Or provide a name within the following list: \n")
+        print("Please provide a number 0-31. Or provide a name within the following list: \n")
         print(colorList)
+
 
 
 def createControl(color_ind):
@@ -34,13 +36,12 @@ def createControl(color_ind):
         cmds.rotate('90deg', 0, 0, r=True)  # fix incorrect rotation
         grp = cmds.group(ctrl, n='Grp_Crtl')
 
-
         colorChange(color_ind, ctrl) # change control's color
 
     elif len_sels == 1: # if there is only one selection, name it
         ctrl_name = sels[0] + "_Ctrl"
         ctrl = cmds.circle(n=ctrl_name)  # create and name nurbs circle
-        cmds.select(x)  # select it
+        cmds.select(ctrl)  # select it
         cmds.rotate('90deg', 0, 0, r=True)  # fix incorrect rotation
         grp = cmds.group(ctrl, n='Grp_Crtl')
         cmds.matchTransform(grp, sels[0]) # set control to match transforms of selection
@@ -49,12 +50,14 @@ def createControl(color_ind):
 
     else:
         for sel in sels:
+            print(sels)
+            print(sel)
             ctrl_name = sel + "_Ctrl"
             ctrl = cmds.circle(n=ctrl_name)  # create and name nurbs circle
-            cmds.select(x)  # select it
+            cmds.select(ctrl[0])  # select it
             cmds.rotate('90deg', 0, 0, r=True)  # fix incorrect rotation
-            grp = cmds.group(ctrl, n='Grp_Crtl')
-            cmds.matchTransform(ctrl, sels[0])  # set control to match transforms of selection
+            grp = cmds.group(ctrl[0], n='Grp_Crtl')
+            cmds.matchTransform(ctrl[0], sel)  # set control to match transforms of selection
 
             colorChange(color_ind, ctrl)  # change control's color
 
